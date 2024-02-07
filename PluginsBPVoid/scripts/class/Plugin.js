079064323730a1661af280034abcd2302c10cff6
QuentinFTL <44733541+QuentinFTL@users.noreply.github.com>
2024-02-07 13:50:09 +0100

    Update Plugin.js

--- PluginsBPVoid/scripts/class/Plugin.js
+++ PluginsBPVoid/scripts/class/Plugin.js
@@ -1,8 +1,9 @@
 import { Player, system, world } from "@minecraft/server"; 
+import { Color } from "./MinecraftConst";
 
 export class Plugin {
     constructor() {
-
+        this.name = this.constructor.name;
     }
 
     add_user_array() {
@@ -15,6 +16,16 @@ export class Plugin {
 
     get_user_array(name, user) {
         return this.uArray[name + user] ?? null;
+    }
+
+    load(pluginPrefix = "") {
+        if(pluginPrefix != "") {
+            world.sendMessage(pluginPrefix + ": Loading plugin...");
+        }
+        else
+        {
+            world.sendMessage("Loading plugin...");
+        }
     }
 
     start(pluginPrefix = "") {
@@ -42,5 +53,24 @@ export class Plugin {
     commands(plugin, json_) {
 
         system.pluginMgr.commandManager.addJson(json_, plugin);
+    }
+
+    missingDependencies(keepPluginAlive, deps) {
+        console.warn("::"+typeof deps);
+        if(typeof deps == "object") {
+            let deps_ = "";
+
+            for (let i = 0; i < deps.length; i++) {
+                const dep = deps[i];
+                deps_ += "\n    - "+dep;
+            }
+
+
+            world.sendMessage(Color.RED + this.name + " need "+deps.length+" missing dependenc"+(deps.length > 1 ? "ies": "y")+": "+ deps_);
+        }
+        else
+        {
+            this.missingDependencies(keepPluginAlive, [deps]);
+        }
     }
 }
